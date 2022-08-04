8d652330e99613585d5d62aa11509a9fc7400c43
swayampandey <swayam265@gmail.com>
2022-08-04 20:53:44 +0530

    MODIFIED: [Swayam] UC-2 player rolls die to get number between 1 to 6

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -1,11 +1,18 @@
 package com.bridgelabz;
 
 public class SnakeAndLadder {
+	/**
+	 * UC-2 --> Player rolls a dice to get number b/w 1 to 
+	 * 
+	 */
+
 
 	public static void main(String[] args) {
 		
 		int position = 0;
 		System.out.println("Position of Player1 is : " +position);
+		int roll = (int)(Math.random()*(6-1))+1;
+		System.out.println("Number after rolling die: " + roll);
 
 
 	}
