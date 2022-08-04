4035c3d1c05bd126f5095e841d903998b0f6adf7
swayampandey <swayam265@gmail.com>
2022-08-04 20:59:37 +0530

    MODIFIED: [Swayam] UC-3 player checking for option program

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -2,17 +2,35 @@ package com.bridgelabz;
 
 public class SnakeAndLadder {
 	/**
-	 * UC-2 --> Player rolls a dice to get number b/w 1 to 
+	 * UC-3 --> Player checking for condition that is Ladder
+	 * snake and no play using  random function 
 	 * 
 	 */
 
 
 	public static void main(String[] args) {
 		
-		int position = 0;
-		System.out.println("Position of Player1 is : " +position);
-		int roll = (int)(Math.random()*(6-1))+1;
-		System.out.println("Number after rolling die: " + roll);
+			int position = 0;
+			int roll = (int)(Math.random()*(6-1))+1;
+			System.out.println("Number after rolling die: " + roll);
+			
+			int option = (int)(Math.random() * 10 ) % 3;
+			System.out.println("Option : "+option);
+			switch (option) {
+			case 1:
+				System.out.println("It's Ladder!!");
+				position +=roll;
+				break;
+			case 2:
+				System.out.println("It's is Snake!!");
+				position -=roll;
+				if(position<0)
+					position = 0;
+				break;
+			default:
+				System.out.println("No Play!!");
+			}
+			System.out.println("Position of the Single player: " + position);
 
 
 	}
